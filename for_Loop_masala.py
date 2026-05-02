c57dd50f2db3ef323834d9ccb76b45e7e5cc424b
XURSANDBEK <sams938693382@gmail.com>
2026-05-02 22:59:25 +0500

    Add files via upload

---
+++ for_Loop_masala.py
@@ -0,0 +1,71 @@
+"""for loop da masala ishlash
+"""
+# oquvchilar = ['Xursandbek','Zafarbrt','Muhammadjon']
+# yillar = [14,14,14]
+# for ism in oquvchilar:
+#     print(f"salom oquvchilar"(ism.title()))
+# print("shart bajaring (a < b)")
+# a = int(input("a soniga butun qiymat kiriting"))
+# b = int(input("b soniga butun qiymat kiriting"))
+# sonlar = list(range(a,b+1))
+# print(sonlar)
+# sonlar.sort(reverse=True)
+# print(sonlar)
+# print(f"Royhatdagi elementlar soni:{len(sonlar)}")
+
+# narx = int(input("1kg kanfet narxini kiriting"))
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} klagram kanfetning narxi{son*narx}so'm")
+
+#7-masala
+
+# a = int(input("A soniga butun qiymat kiriting :"))
+# b = int(input("B soniga butun qiymat kiriting :"))
+
+# print(f"A ning malumot tipi = {type(a)}")
+# print(f"Bning malumot tipi = {type(b)}")
+
+# sonlar = list(range(a,b+1))
+# print("Royhat : {sonlar}")
+# kopaytma = 1
+# for son in sonlar:
+#     kopaytma *= son
+#     print(f"Royhatning elementlarni kopaytmasi : {kopaytma}")
+
+
+#9-masala
+# print("a < b")
+# a= int(input("A soniga butun qiymat kiriting :"))
+# b = int(input("B soniga butun qiymat kiriting :"))
+
+# sonlar = list(range(a,b+1))
+# print("Royhat : {sonlar}")
+# kvadrat = []
+# yigindisi = 0
+# for son in sonlar:
+#     kvadrat.append(son**2)
+#     yigindisi += son**2
+# print(f"Darajaga oshirish royhat : {kvadrat}")
+# print(f"Darajaga oshirish royhat elementlari yigindisi :{yigindisi}")
+
+#10-masala
+
+n= int(input("n soniga butun qiymat kiriting :"))
+
+sonlar = list(range(1,n+1))
+korinish = []
+for son in sonlar:
+    i = f"1/{son}"
+    korinish.append(i)
+    i = 0
+    yigindi += 1/son
+print(korinish)
+print(f"Yigndi : {yigindi}")
+
+
+ 
+
+    
+
